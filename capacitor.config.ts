7567dc3fbf819864294e26172aa5e64159a86889
Liam <2313988763@qq.com>
2024-10-28 12:26:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minimall-web',
+  webDir: 'dist'
+};
+
+export default config;
